2f81fa8cfffa8411fbf94e71971b47fa7e42c644
jeffrey <piccirilloj1@gmail.com>
2022-09-10 17:01:58 -0500

    Abstract results table's boundaries into mixin so they can be reused in the Chart

--- src/services/mixins.js
+++ src/services/mixins.js
@@ -13,6 +13,27 @@ export default [
       closeModalFromBus() {
         bus.$emit(MODAL_CLOSED);
       },
+      isMild({ sum, average }) {
+        return {
+          sum: sum <= 29,
+          average: average <= 1,
+          phrase: { sum: '≤ 29 ', average: '≤ 1' },
+        };
+      },
+      isModerate({ sum, average }) {
+        return {
+          sum: sum > 29 && sum <= 58,
+          average: average > 1.01 && average <= 2,
+          phrase: { sum: '29 to 58', average: '1.01 to 2' },
+        };
+      },
+      isSevere({ sum, average }) {
+        return {
+          sum: sum > 58,
+          average: average > 2,
+          phrase: { sum: '> 58 ', average: '> 2' },
+        };
+      },
     },
   },
 ];
